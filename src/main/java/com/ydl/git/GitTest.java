3121883f855edb9753663db6974d90aab2cd385d
WangCheng <1733239547@qq.com>
2022-12-25 09:25:23 +0800

    first commit

---
+++ src/main/java/com/ydl/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.ydl.git;
+
+/**
+ * @Author by IT王城
+ * @date 2022/12/25
+ * @apiNote
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
